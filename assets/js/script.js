74076882e5170a1eb42f5bdb16770c260529c1e4
raxemmohamed <raxemmohamed@gmail.com>
2019-06-06 07:58:25 -0500

    commit train 1

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDQ4w9j6afDbePhu-WH9gu3JGZKiDBVSAo",
+    authDomain: "clicker516.firebaseapp.com",
+    databaseURL: "https://clicker516.firebaseio.com",
+    projectId: "clicker516",
+    storageBucket: "clicker516.appspot.com",
+    messagingSenderId: "516025280332",
+    appId: "1:516025280332:web:6e4789d68956c445"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database();
+
+
